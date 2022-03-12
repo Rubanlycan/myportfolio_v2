37188c66cea0dbfbc5e1724e7897013b4fd21587
Ruban <ruban@prescrip.in>
2022-03-13 01:21:53 +0530

    analytics responsive  added

--- src/Components/NoMobileResponsiveComponent.js
+++ src/Components/NoMobileResponsiveComponent.js
@@ -5,7 +5,8 @@ const NoMobileResponsiveComponent = () => {
         <div className='under_dev'>
             <img src={require('../assets/web_development_construction.gif')} style={{ width: '100%', height: 200 }} alt="under_construction" />
             <h2 className='text-center m-5' style={{ color: '#fff' }}>
-                Mobile Responsive under development...</h2></div>
+                Please view on Desktop/Laptop   </h2>
+            <span className='text-center' style={{ color: '#fff', fontSize: 18 }}> Mobile Responsive under development...</span></div>
     )
 }
 
